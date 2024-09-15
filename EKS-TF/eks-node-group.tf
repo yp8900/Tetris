d9f12e716a1bb471be2f0ec4cf5af7b56a6ca441
yp8900 <150072487+yp8900@users.noreply.github.com>
2024-09-15 09:30:11 +0530

    Update eks-node-group.tf

--- EKS-TF/eks-node-group.tf
+++ EKS-TF/eks-node-group.tf
@@ -12,7 +12,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   }
 
   ami_type       = "AL2_x86_64"
-  instance_types = ["t2.medium"]
+  instance_types = ["t3.microt2"]
   disk_size      = 20
 
   depends_on = [
